c78e98bebb33572521e581d66c6bd2090468c9cb
Christian Bollmann <cbollmann@stud.hs-bremen.de>
2013-12-02 12:34:55 +0100

    initial eclipse project commit

---
+++ balance-it-desktop/src/de/hsbremen/mobile/balanceit/Main.java
@@ -0,0 +1,16 @@
+package de.hsbremen.mobile.balanceit;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "balance-it";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new BaseGame(), cfg);
+	}
+}
