ee8e67c1e82f85d664a3fd79b37bc3f9dc16d86e
Christian Bollmann <cbollmann@stud.hs-bremen.de>
2014-01-20 11:22:27 +0100

    added fitting models

--- balance-it-android/src/de/hsbremen/mobile/balanceit/MainActivity.java
+++ balance-it-android/src/de/hsbremen/mobile/balanceit/MainActivity.java
@@ -4,15 +4,34 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.example.games.basegameutils.GameHelper;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
-public class MainActivity extends AndroidApplication {
-    @Override
+public class MainActivity extends AndroidApplication implements GameHelperListener {
+    private GameHelper gameHelper;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        gameHelper = new GameHelper(this);
+        gameHelper.enableDebugLog(true, "BIPS");
+        
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
         
         initialize(new BaseGame(), cfg);
     }
+
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- balance-it/src/de/hsbremen/mobile/balanceit/BaseGame.java
+++ balance-it/src/de/hsbremen/mobile/balanceit/BaseGame.java
@@ -2,13 +2,25 @@ package de.hsbremen.mobile.balanceit;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class BaseGame implements ApplicationListener {
 	private OrthographicCamera camera;
@@ -16,40 +28,89 @@ public class BaseGame implements ApplicationListener {
 	private Texture texture;
 	private Sprite sprite;
 	
+	private PerspectiveCamera cam;
+	private Model model;
+	private Model ball;
+	private ModelInstance instance;
+	private ModelInstance instance2;
+	private ModelBatch modelBatch;
+	private Environment environment;
+	private CameraInputController camController;
+	
 	@Override
-	public void create() {		
+	public void create() {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight,0.4f,0.4f,0.4f,1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		
+		modelBatch = new ModelBatch();
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 0.1f;
+		cam.far = 300f;
+		cam.update();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createCylinder(20f, 0.2f, 20f, 32, 
+				new Material(ColorAttribute.createDiffuse(Color.YELLOW)), 
+				Usage.Position | Usage.Normal);
+//		model = modelBuilder.createBox(5f, 5f, 5f, 
+//				new Material(ColorAttribute.createDiffuse(Color.YELLOW)), 
+//				Usage.Position | Usage.Normal);
+		instance = new ModelInstance(model);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		ball = modelBuilder.createSphere(3f, 3f, 3f, 32, 32, 
+				new Material(ColorAttribute.createDiffuse(Color.RED)),
+				Usage.Position | Usage.Normal);
+		
+		instance2 = new ModelInstance(ball);
+		
+//		camera = new OrthographicCamera(1, h/w);
+//		batch = new SpriteBatch();
+//		
+//		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+//		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//		
+//		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+//		
+//		sprite = new Sprite(region);
+//		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+//		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+//		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+//		batch.dispose();
+//		texture.dispose();
+		model.dispose();
+		modelBatch.dispose();
 	}
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		camController.update();
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		
+		modelBatch.begin(cam);
+		modelBatch.render(instance,environment);
+		modelBatch.render(instance2,environment);
+		modelBatch.end();
+		
+//		batch.setProjectionMatrix(camera.combined);
+//		batch.begin();
+//		sprite.draw(batch);
+//		batch.end();
 	}
 
 	@Override
